db20c4596018f9ed24842602826d7ba308bd004c
OleksandrVelychko <a.velichko1983@gmail.com>
2022-01-19 00:11:03 +0200

    Students model modified

--- students/models.py
+++ students/models.py
@@ -6,4 +6,4 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=64, null=False)
     last_name = models.CharField(max_length=64, null=False)
-    age = models.IntegerField(null=True)
+    age = models.IntegerField(default=20)
