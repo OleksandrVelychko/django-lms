788d9b2bb3303e5743f23e8c81ecb8989c70b510
OleksandrVelychko <a.velichko1983@gmail.com>
2022-01-23 10:40:34 +0200

    Created view function for filtering students, Added flake8 and updated gitignore for .flake8

--- students/models.py
+++ students/models.py
@@ -1,7 +1,22 @@
 from django.db import models
+from faker import Faker
+import random
 
 
 class Student(models.Model):
     first_name = models.CharField(max_length=64, null=False)
     last_name = models.CharField(max_length=64, null=False)
     age = models.IntegerField(default=20)
+
+    @classmethod
+    def generate_students(cls, count):
+        faker = Faker()
+        for _ in range(count):
+            s = Student()
+            s.first_name = faker.first_name()
+            s.last_name = faker.last_name()
+            s.age = random.randint(18, 80)
+            s.save()
+
+    def __str__(self):
+        return f"Student({self.id}) {self.first_name} {self.last_name} {self.age}"
--- students/views.py
+++ students/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render  # noqa
+from django.http import HttpResponse
+from students.models import Student
+from students.utils import render_list
 
-# Create your views here.
+
+def get_students(request):
+    qs = Student.objects.all()
+    params = ['first_name', 'last_name', 'age']
+    query = {}
+
+    for param_name in params:
+        param_value = request.GET.get(param_name)
+        if param_value:
+            query[param_name] = param_value
+
+    try:
+        qs = qs.filter(**query)
+    except ValueError as e:
+        return HttpResponse(f"Error: incorrect data was passed in query string. Details: {str(e)}", status=400)
+
+    # first_name = request.GET.get('first_name')
+    # if first_name:
+    #     qs = qs.filter(first_name = first_name)
+    #
+    # last_name = request.GET.get('last_name')
+    # if last_name:
+    #     qs = qs.filter(last_name=last_name)
+    #
+    # age = request.GET.get('age')
+    # if age:
+    #     qs = qs.filter(age=age)
+
+    return render_list(qs)
