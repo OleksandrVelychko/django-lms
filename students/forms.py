32cf5b916e923670c413c64a147e5e1f4a2df086
OleksandrVelychko <a.velichko1983@gmail.com>
2022-01-28 01:23:19 +0200

    added fields to students model and applied migration

--- students/forms.py
+++ students/forms.py
@@ -5,4 +5,4 @@ from students.models import Student
 class StudentCreateForm(ModelForm):
     class Meta:
         model = Student
-        fields = ['first_name', 'last_name', 'age']
+        fields = '__all__'
