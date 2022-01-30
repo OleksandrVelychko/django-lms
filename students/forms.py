78388b7610ccbe660bc10ae1dffb1cd2ace592ea
OleksandrVelychko <a.velichko1983@gmail.com>
2022-01-30 22:20:54 +0200

    Added phone number check for uniqueness for Students

--- students/forms.py
+++ students/forms.py
@@ -1,5 +1,3 @@
-import re
-
 from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 from students.models import Student
@@ -10,12 +8,6 @@ class StudentBaseForm(ModelForm):
         model = Student
         fields = '__all__'
 
-    def clean_phone_number(self):
-        phone_number = self.cleaned_data['phone_number']
-        if not re.match(r'(\+\d\d?)?\(\d{3}\)(\d-?){7}$', phone_number):
-            raise ValidationError("Phone number should be in format +1(111)111-11-11")
-        return phone_number
-
     def clean(self):
         result = super().clean()
         enroll_date = self.cleaned_data['enroll_date']
@@ -24,6 +16,15 @@ class StudentBaseForm(ModelForm):
         if enroll_date > graduate_date:
             raise ValidationError("Enroll date can't be less than graduate date")
         return result
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data['phone_number']
+        has_phone_number_qs = Student.objects.filter(phone_number=phone_number)
+        if self.instance:
+            has_phone_number_qs = has_phone_number_qs.exclude(id=self.instance.id)
+        if has_phone_number_qs.exists():
+            raise ValidationError("Phone number is not unique")
+        return phone_number
 
 
 class StudentCreateForm(StudentBaseForm):
