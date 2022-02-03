a17735e75ea91e09068284e990fc2553b0ef4d90
OleksandrVelychko <a.velichko1983@gmail.com>
2022-02-03 14:49:40 +0200

    changed page title: overriding in templates instead of using variable

--- core_lms/views.py
+++ core_lms/views.py
@@ -2,8 +2,4 @@ from django.shortcuts import render
 
 
 def get_index_page(request):
-    page_title = 'Main page'
-    return render(request, 'core_lms/index.html', {
-        'args': request.GET,
-        'page_title': page_title
-    })
+    return render(request, 'core_lms/index.html')
