198470d72c0aeaac88dc3b6ef0c73bbd1fca4ec2
OleksandrVelychko <a.velichko1983@gmail.com>
2022-02-03 14:12:53 +0200

    added index page with LMS menu, removed action from forms

--- core_lms/views.py
+++ core_lms/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render  # noqa
+from django.shortcuts import render
 
-# Create your views here.
+
+def get_index_page(request):
+    page_title = 'Main page'
+    return render(request, 'core_lms/index.html', {
+        'args': request.GET,
+        'page_title': page_title
+    })
