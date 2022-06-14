1c365162abcf7f82575434e7a07b3c1a2a970ef8
OleksandrVelychko <a.velichko1983@gmail.com>
2022-06-14 21:13:26 +0300

    added group update view function and form

--- groups/urls.py
+++ groups/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from groups.views import get_group
-
+from groups.views import get_group, update_group
 
 app_name = 'groups'
 
 urlpatterns = [
     path('<int:id>', get_group, name='get_group'),
+    path('update/<int:id>', update_group, name='edit_group'),
     ]
--- groups/views.py
+++ groups/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404  # noqa
+from django.urls import reverse
 
+from groups.forms import GroupUpdateForm
 from groups.models import Group
 
 
@@ -11,3 +14,19 @@ def get_group(request, id):
                       'group': group
                   }
                   )
+
+
+def update_group(request, id):
+    group = get_object_or_404(Group, id=id)
+    if request.method == 'POST':
+        form = GroupUpdateForm(request.POST, instance=group)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('groups:edit_group', kwargs={'id': group.id}))
+    else:
+        form = GroupUpdateForm(instance=group)
+
+    return render(request, 'groups/edit_group.html', {
+        'form': form,
+        'group': group
+    })
